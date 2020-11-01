215c0a76567de790b69c94e17d122bb314c441a0
AkbarXelion <bangkitakbar37@gmail.com>
2020-11-01 13:08:33 +0700

    Update Folder

---
+++ Lab 1.py
@@ -0,0 +1,16 @@
+print("Lab 1")
+print()
+print("A" , end ="")
+print("B" , end ="")
+print("C" , end ="")
+print()
+print("X")
+print("Y")
+print("Z")
+
+w, x, y, z = 10, 15, 20, 25
+print(w, x, y, z)
+print(w, x, y, z, sep = ",")
+print(w, x, y, z, sep = "")
+print(w, x, y, z, sep = ":")
+print(w, x, y, z, sep = "-----")
---
+++ Lab 2.py
@@ -0,0 +1,14 @@
+print("Lab 2")
+print()
+
+
+a = int(input("Masukkan Nilai A :"))
+b = int(input("Masukkan Nilai B :"))
+print("Variable A : ",a)
+print("Variable B : ",b)
+print("Hasil Penggabungan {1}&{0}=%d".format(a,b) %(a+b))
+
+a=int(a)
+b=int(b)
+print("Hasil Penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("Hasil Pembagian {1}/{0}=%d".format(a,b) %(a/b))
